f2ccb849a0952f2667661ddc4b39cce70595c9d5
Braden East <bradeneastdesign@gmail.com>
2021-05-24 08:20:58 -0500

    add more meta tags to head

--- ingrid.config.js
+++ ingrid.config.js
@@ -4,6 +4,8 @@ module.exports = {
 	ignorePattern: /^_/, // By default, files and directories whose names begin with an underscore "_" are ignored
 	global: {
 		SiteName: "Oak Studio",
-		SiteRoot: "https://oakstudio.co"
+		SiteRoot: "https://oakstudio.co",
+		Twitter: "@oakstudioco",
+		PrimaryColor: "#FE3B1F"
 	}, // Key/Value pairs that can be interpolated anywhere in your site using starburns syntax: {* ExampleValue *} or used in an expression with nunjucks syntax: {% condition ? ExampleValue : OtherValue %}
 }
