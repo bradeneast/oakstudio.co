ee7ca1ca12ad56166a1c76a6b2e49101f9b168a6
Braden East <bradeneastdesign@gmail.com>
2021-07-03 17:46:19 -0500

    adjust image compression options

--- prebuild/_options.js
+++ prebuild/_options.js
@@ -19,4 +19,4 @@ export let targetExt = "webp";
 export let matchExts = new RegExp(`(${validExts.join('|')})$`, 'i');
 export let matchSrc = /(?<= (src=|url\()(["'`])).+?(?=\2[ >\/\)])/gi;
 export let resizeOptions = [2160, 2160, { fit: "inside", withoutEnlargement: true }];
-export let formatOptions = { quality: 80, reductionEffort: 5 };
+export let formatOptions = { quality: 70, reductionEffort: 2 };
