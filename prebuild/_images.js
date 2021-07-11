6eeb6695fe41efa298031f15ae5b8476715255c0
Braden East <bradeneastdesign@gmail.com>
2021-07-11 16:15:06 -0500

    add writing to cache file in github repo

--- prebuild/_images.js
+++ prebuild/_images.js
@@ -1,11 +1,13 @@
 import fs from 'fs-extra';
 import sharp from 'sharp';
-import colors from 'colors';
 import { join, normalize } from 'path';
+import colors from 'colors';
+import { logFile } from './_utils.js';
+import * as repo from './_repo.js';
 import { outDir, targetExt, formatOptions, resizeOptions, matchExts, cacheFile } from './_options.js';
 
-async function logFile(message, filename) {
-  console.log(colors.gray(message + ":"), filename);
+function makeOutPath(filename) {
+  return join(outDir, filename.replace(matchExts, targetExt))
 }
 
 /**Resize, compress, and format an image, then write it to a new file */
@@ -17,10 +19,10 @@ async function resizeImage(src, dest, filename) {
       .toFormat(targetExt)
       .webp(formatOptions)
       .toFile(dest)
-      .then(() => logFile("📐 RSZD", filename))
+      .then(() => logFile("📐 RESIZED", filename))
       .catch(console.error)
   } catch (err) {
-    console.log("Error resizing ".red + src);
+    console.log(colors.red("Error resizing ") + src);
     console.error(err);
   }
 }
@@ -28,7 +30,7 @@ async function resizeImage(src, dest, filename) {
 /**Iterates over a directory and processes each image file
  * Outputs to a subfolder within the source directory
  */
-async function processImages(dir, useCache = false) {
+async function processImages(dir) {
 
   let dirName = normalize(dir);
   let images = fs.readdirSync(dirName);
@@ -36,48 +38,51 @@ async function processImages(dir, useCache = false) {
   let diff = cache.images.filter(image => !images.includes(image));
 
   // Diff cached images with images present
-  if (diff.length && useCache)
-    for (let filename of diff) {
-
-      // Remove filename from cache
-      cache.images.splice(cache.images.indexOf(filename), 1);
-
-      // Remove file from output directory
-      fs.remove(join(outDir, filename.replace(matchExts, targetExt)))
-        .then(() => logFile("🗑️ Removed cached file", filename));
-    }
-
-  // Process uncached images
-  images
-    .filter(image => cache.images.includes(image))
-    .map(processImage);
-
-  /**Generates src and dest paths for an image file and resizes or copies it */
-  async function processImage(filename) {
-
-    let src = join(dirName, filename);
-    let dest = join(outDir, filename.replace(matchExts, targetExt));
-
-    // Return if file is directory
-    if (fs.lstatSync(src).isDirectory()) return;
-    // Return if file is cached
-    if (useCache && cache.images.includes(filename))
-      return logFile("Using cached file", filename);
-
-    // Resize if file is supported format : else copy file
-    if (matchExts.test(filename))
-      resizeImage(src, dest, filename)
-    else {
-      fs.ensureFileSync(dest);
-      fs.copyFile(src, dest).then(() => logFile("📄 COPD", filename))
-    }
-
-    // Add filename to cache
-    if (useCache) cache.images.push(filename);
+  for (let filename of diff) {
+    // Remove filename from cache
+    cache.images.splice(cache.images.indexOf(filename), 1);
+    // Remove file from output directory
+    fs.remove(makeOutPath(filename))
+      .then(() => logFile("🗑️ REMOVED", filename));
   }
 
-  // Write updated cache to cache file
-  if (useCache) fs.writeFile(cacheFile, JSON.stringify(cache));
+  // Process images
+  images.map(processImage);
+
+  // Update cache files
+  const stringifiedCache = JSON.stringify(cache);
+  fs.writeFileSync(cacheFile, stringifiedCache);
+  repo
+    .updateFileContents(cacheFile, stringifiedCache)
+    .then(() => logFile("Cache updated", cacheFile))
+
+
+  /**Generates src and dest paths for an image file and resizes or copies it */
+  function processImage(filename) {
+
+    let src = join(dirName, filename);
+    let dest = makeOutPath(filename);
+
+    // Return if file is directory
+    if (fs.lstatSync(src).isDirectory())
+      return;
+    // Return if file is cached
+    if (cache.images.includes(filename)) {
+      logFile("Using cached file", filename);
+      return;
+    }
+    // Resize if file is supported format
+    if (matchExts.test(filename)) {
+      resizeImage(src, dest, filename);
+    }
+    // Copy if file is not supported format
+    else {
+      fs.ensureFileSync(dest);
+      fs.copyFile(src, dest).then(() => logFile("📄 COPIED", filename))
+    }
+    // Add filename to cache
+    cache.images.push(filename);
+  }
 }
 
 export default processImages;
---
+++ prebuild/_repo.js
@@ -0,0 +1,40 @@
+import octokit from "octokit";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { Octokit } = octokit;
+const client = new Octokit({ auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN });
+const owner = "bradeneast";
+const repo = "oakstudio.co";
+
+export async function updateFileContents(path, contents) {
+  const file = await getFile(path);
+  const content = new Buffer.from(contents, 'utf8').toString('base64');
+  return client.rest.repos
+    .createOrUpdateFileContents({
+      owner,
+      repo,
+      path,
+      content,
+      sha: file.data.sha,
+      message: `[skip ci] Update ${path} from octokit NodeJS client`,
+    })
+    .catch(console.error)
+}
+
+export async function getFile(path) {
+  return client.rest.repos
+    .getContent({
+      accept: "application/vnd.github.v3+json",
+      owner,
+      repo,
+      path,
+    })
+    .catch(console.error)
+}
+
+export async function getFileContents(path) {
+  const res = await getFile(path);
+  const buff = new Buffer.from(res.data.content, 'base64');
+  return buff.toString('utf8');
+}
