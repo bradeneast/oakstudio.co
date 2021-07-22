129eaef89e4891f587c0fd261d4a6d06ed7ffc04
Braden East <bradeneastdesign@gmail.com>
2021-07-22 17:28:05 -0500

    move images and change resize scale

--- prebuild/_options.js
+++ prebuild/_options.js
@@ -12,12 +12,12 @@ let validExts = [
 ];
 export let siteSrc = "_src";
 export let imageDirName = "img";
-export let outDirName = "sm";
+export let outDirName = "s";
 export let cacheFile = "prebuild/cache.json";
 export let inDir = join(siteSrc, imageDirName);
 export let outDir = join(siteSrc, imageDirName, outDirName);
 export let targetExt = "webp";
 export let matchExts = new RegExp(`(${validExts.join('|')})$`, 'i');
 export let matchSrc = /(?<= (src=|url\()(["'`])).+?(?=\2[ >\/\)])/gi;
-export let resizeOptions = [2160, 2160, { fit: "inside", withoutEnlargement: true }];
-export let formatOptions = { quality: 70, reductionEffort: 2 };
+export let resizeOptions = [1440, 1440, { fit: "inside", withoutEnlargement: true }];
+export let formatOptions = { quality: 50, reductionEffort: 2 };
