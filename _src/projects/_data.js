b720e5d1d3cdde0ced0d95d1e09a2c764dd9edd7
Braden East <bradeneastdesign@gmail.com>
2021-07-23 17:41:53 -0500

    add areaDescription velue for head meta

--- _src/projects/_data.js
+++ _src/projects/_data.js
@@ -2,6 +2,7 @@ export const type = "project";
 export const layout = "layouts/project.njk";
 export const templateEngine = "njk,md";
 export const bodyClass = "project";
+export const areaDescription = "Case studies of our work here at Oak Studio";
 
 export function url(page) {
   return `/projects/${page.data.title}/`
